2ba3e78d01b1ce7f436b7acdd3530b5187f827a2
Drileba Elvis Edward <yokokochristine@gmail.com>
2026-03-15 21:42:39 +0300

    Removed unnecessary folders, keeping payment_api only

--- day_5/test_logger.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import patch
-import logger
-
-@patch('logger.log_warning', wraps=logger.log_warning)
-def test_input_validator_logs_warning_for_empty_input(logs):
-    validator = logger.InputValidator()
-    result = validator.validate("")
-    assert result == False
-    logs.assert_called_with("Empty input received!")
